94a16be436ce4b8c3eebcf59fbd46044306e4564
Harlit Estevez <harlit.estevez@gmail.com>
2022-09-11 22:53:03 -0400

    Se agraga el proyecto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+public class Contexto : DbContext
+    {
+        public DbSet<Personas> Personas { get; set; }
+
+    public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+        }
+
+    public Contexto()
+    {
+    }
+}
